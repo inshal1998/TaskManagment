253ccae980387367ba2816b7b1e947a48a1341e0
inshal@8848digital.com <inshal@8848digital.com>
2024-12-17 10:51:23 +0530

    configuration updated

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors()); 
-app.use(express.json()); 
+app.use(cors());
+app.use(express.json());
 
-app.use("/tasks", taskRoutes); 
+// Use task routes
+app.use("/tasks", taskRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
